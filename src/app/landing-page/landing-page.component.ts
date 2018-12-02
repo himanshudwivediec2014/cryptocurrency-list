89c903c3c08ff5bfaf7e3cd5e49cd1a2ed37283e
Himanshu Dwivedi <himanshudwivediec2014@gmail.com>
2018-12-02 15:37:30 +0530

    Final Commit

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import * as  particlesJS from 'particles.js';
+declare var particlesJS: any;
 
 @Component({
   selector: 'landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    particlesJS.load('particles-js', 'assets/data/particles.json',
+     function() { 
+       console.log('callback - particles.js config loaded'); 
+      });
   }
 
 }
